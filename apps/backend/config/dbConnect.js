a3e616c486bdbc7e09b53fcd8eb5af42ffdfb32c
ravi sankar <ravi.sankar@aquera.com>
2025-06-26 15:48:02 +0530

    sever db

--- apps/backend/config/dbConnect.js
+++ apps/backend/config/dbConnect.js
@@ -1,27 +1,12 @@
-const mongoose=require("mongoose");
-
+const mongoose = require("mongoose");
 
 const dbConnect = async () => {
   try {
-    await mongoose.connect("mongodb://onstepadmin:@Agent000000@3.111.58.3:27017/onstepdb", {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      authSource: "admin",
-    });
+    await mongoose.connect("mongodb://onstepadmin:%40Agent000000@3.111.58.3:27017/onstepdb?authSource=admin");
     console.log("✅ MongoDB connected");
   } catch (error) {
     console.error("❌ MongoDB connection failed:", error.message);
   }
 };
 
-// const dbConnect=async()=>{mongoose.connect("mongodb+srv://arunkumarveerapandian4:RnLrgUqPl9gYe3E1@service.uiitzln.mongodb.net/?retryWrites=true&w=majority&appName=service/test")};
-
-
-//arunkumarveerapandian4
-//RnLrgUqPl9gYe3E1
-
-
-//mongodump --uri="mongodb+srv://arunkumarveerapandian4:RnLrgUqPl9gYe3E1@service.uiitzln.mongodb.net/?retryWrites=true&w=majority&appName=service/rentalDB" --db=mydatabase --archive=mydatabase.archive.gz --gzip
-
-
-module.exports=dbConnect;
+module.exports = dbConnect;
