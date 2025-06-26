e25981460b1ac37892f46f4a42eaed9bc63fefbe
ravi sankar <ravi.sankar@aquera.com>
2025-06-26 15:43:15 +0530

    db fixes

--- apps/backend/config/dbConnect.js
+++ apps/backend/config/dbConnect.js
@@ -1,7 +1,22 @@
 const mongoose=require("mongoose");
 
 
-const dbConnect=async()=>{mongoose.connect("mongodb+srv://arunkumarveerapandian4:RnLrgUqPl9gYe3E1@service.uiitzln.mongodb.net/?retryWrites=true&w=majority&appName=service/test")};
+const dbConnect = async () => {
+  try {
+    await mongoose.connect("mongodb://onstepadmin:@Agent000000@3.111.58.3:27017/onstepdb", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      authSource: "admin",
+    });
+    console.log("✅ MongoDB connected");
+  } catch (error) {
+    console.error("❌ MongoDB connection failed:", error.message);
+  }
+};
+
+// const dbConnect=async()=>{mongoose.connect("mongodb+srv://arunkumarveerapandian4:RnLrgUqPl9gYe3E1@service.uiitzln.mongodb.net/?retryWrites=true&w=majority&appName=service/test")};
+
+
 //arunkumarveerapandian4
 //RnLrgUqPl9gYe3E1
 
