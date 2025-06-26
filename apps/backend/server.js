690696e3daaf86fe7c75f8005b24c924053940bb
ravi sankar <ravi.sankar@aquera.com>
2025-06-26 15:02:37 +0530

    orgin fixes

--- apps/backend/server.js
+++ apps/backend/server.js
@@ -12,7 +12,7 @@ const transRouter = require("./routes/transactionRoute");
 const app = express();
 app.use(cookieParser());
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: true,
   credentials: true,
 }));
 app.use(express.json());
