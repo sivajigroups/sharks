d76cbcc9fc58800d8820758d38c1b1457b6fdd46
ARUNKUMAR016 <168518865+ARUNKUMAR016@users.noreply.github.com>
2025-07-08 13:26:45 +0530

    cors

--- apps/backend/server.js
+++ apps/backend/server.js
@@ -11,41 +11,41 @@ const adminRouter = require("./routes/adminRoute");
 const transRouter = require("./routes/transactionRoute");
 const app = express();
 app.use(cookieParser());
-const allowedOrigins = [
-  "http://localhost:5173",
-  "https://sharks.sivajigroups.com",
-];
+// const allowedOrigins = [
+//   "http://localhost:5173",
+//   "https://sharks.sivajigroups.com",
+// ];
 
-app.use((req, res, next) => {
-  const origin = req.headers.origin;
+// app.use((req, res, next) => {
+//   const origin = req.headers.origin;
 
-  // Detect Nginx-set CORS header (res.getHeaders() contains all response headers)
-  const headers = res.getHeaders();
-  const alreadySet = Object.keys(headers).some(
-    (key) => key.toLowerCase() === "access-control-allow-origin"
-  );
+//   // Detect Nginx-set CORS header (res.getHeaders() contains all response headers)
+//   const headers = res.getHeaders();
+//   const alreadySet = Object.keys(headers).some(
+//     (key) => key.toLowerCase() === "access-control-allow-origin"
+//   );
 
-  // Only set CORS headers if not already set
-  if (!alreadySet && origin && allowedOrigins.includes(origin)) {
-    res.setHeader("Access-Control-Allow-Origin", origin);
-    res.setHeader("Access-Control-Allow-Credentials", "true");
-    res.setHeader(
-      "Access-Control-Allow-Headers",
-      "Origin, Content-Type, Accept, Authorization"
-    );
-    res.setHeader(
-      "Access-Control-Allow-Methods",
-      "GET, POST, OPTIONS, PUT, DELETE, PATCH"
-    );
-  }
+//   // Only set CORS headers if not already set
+//   if (!alreadySet && origin && allowedOrigins.includes(origin)) {
+//     res.setHeader("Access-Control-Allow-Origin", origin);
+//     res.setHeader("Access-Control-Allow-Credentials", "true");
+//     res.setHeader(
+//       "Access-Control-Allow-Headers",
+//       "Origin, Content-Type, Accept, Authorization"
+//     );
+//     res.setHeader(
+//       "Access-Control-Allow-Methods",
+//       "GET, POST, OPTIONS, PUT, DELETE, PATCH"
+//     );
+//   }
 
-  // Preflight
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(204);
-  }
+//   // Preflight
+//   if (req.method === "OPTIONS") {
+//     return res.sendStatus(204);
+//   }
 
-  next();
-});
+//   next();
+// });
 
 app.use(express.json());
 app.use("/api", userRouter);
