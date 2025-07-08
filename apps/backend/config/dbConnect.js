fc2bd8f0c8fe41931a5f2d4e721f09e9b42a53aa
ARUNKUMAR016 <168518865+ARUNKUMAR016@users.noreply.github.com>
2025-07-08 11:50:05 +0530

    env

--- apps/backend/config/dbConnect.js
+++ apps/backend/config/dbConnect.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
 
 const dbConnect = async () => {
   try {
-    await mongoose.connect("mongodb://onstepadmin:%40Agent000000@3.111.58.3:27017/onstepdb?authSource=admin");
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("✅ MongoDB connected");
   } catch (error) {
     console.error("❌ MongoDB connection failed:", error.message);
