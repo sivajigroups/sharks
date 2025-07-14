30535fa7168451cbc82bc2a3cd496895f17d8224
ravi sankar <ravi.sankar@aquera.com>
2025-07-14 14:38:33 +0530

    env fix

--- apps/backend/config/dbConnect.js
+++ apps/backend/config/dbConnect.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 const dbConnect = async () => {
   try {
     console.log(process.env.MONGO_URI);
-    await mongoose.connect('mongodb://onstepadmin:%40Agent000000@3.111.58.3:27017/onstepdb');
+    await mongoose.connect(`mongodb://onstepadmin:%40Agent000000@3.111.58.3:27017/onstepdb`);
     console.log("✅ MongoDB connected");
   } catch (error) {
     console.error("❌ MongoDB connection failed:", error.message);
