9ae97078f8e20cb42ff4ff0da6721c4db4c25c0e
ravi sankar <ravi.sankar@aquera.com>
2025-07-14 15:04:38 +0530

    db fixes

--- apps/backend/config/dbConnect.js
+++ apps/backend/config/dbConnect.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const MONGO_URI = `mongodb://onstepadmin:%40Agent000000@3.111.58.3:27017/admin`;
+const MONGO_URI = `mongodb://onstepadmin:%40Agent000000@3.111.58.3:27017/onstepdb?authSource=admin`;
 
 const dbConnect = async () => {
   try {
